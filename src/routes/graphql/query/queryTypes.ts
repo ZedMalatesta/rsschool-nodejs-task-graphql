51bc9e36742a3815115662ab266ef11d79d2336b
Pavel <opatriarkh@gmail.com>
2023-07-24 08:25:05 +0300

    feat: add users and profiles queries with some bugs

--- src/routes/graphql/query/queryTypes.ts
+++ src/routes/graphql/query/queryTypes.ts
@@ -2,6 +2,7 @@ import {
     GraphQLList,
     GraphQLObjectType,
     GraphQLString,
+    GraphQLBoolean,
     GraphQLNonNull,
     GraphQLInt,
     GraphQLFloat,
@@ -10,14 +11,18 @@ import {
 import {
   getAllMemberTypes, 
   getAllPosts,
-  getPostByID
+  getAllProfiles,
+  getAllUsers,
+  getPostByID,
+  getUserByID,
+  getPostsByUserID,
+  getProfileByUserID,
 } from './resolvers.js'
 import { ContextInterface } from '../types/types.js';
 import { UUIDType } from '../types/uuid.js'; 
 
 const memberTypeEnum = new GraphQLEnumType({
     name: 'memberType',
-    description: 'memberType',
     values: {
         BASIC: {
             value: 'basic'
@@ -42,10 +47,46 @@ const postType = new GraphQLObjectType({
   fields: () => ({
     id: { type: new GraphQLNonNull(UUIDType) },
     title: { type: new GraphQLNonNull(GraphQLString) },
-    content: { type: new GraphQLNonNull(GraphQLString) }
+    content: { type: new GraphQLNonNull(GraphQLString) },
+    authorId: { type: UUIDType }
   }),
 });
 
+export const profileType = new GraphQLObjectType({
+  name: 'Profile',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(UUIDType) },
+    isMale: { type: new GraphQLNonNull(GraphQLBoolean) },
+    yearOfBirth: { type: new GraphQLNonNull(GraphQLInt) },
+    memberTypeId: { type: new GraphQLNonNull(memberTypeEnum) },
+    userId: { type: UUIDType },
+  }),
+});
+
+export const userType: GraphQLObjectType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(UUIDType) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    balance: { type: new GraphQLNonNull(GraphQLFloat) },
+    posts: {
+      type: new GraphQLList(postType),
+      resolve: (
+        parent: { userId: string }, 
+        args, 
+        context:ContextInterface
+      ) => getPostsByUserID(parent, args, context)
+    },
+    proflie: {
+      type: profileType,
+      resolve: (
+        parent: { userId: string }, 
+        args, 
+        context:ContextInterface
+      ) => getProfileByUserID(parent, args, context)
+    }
+  }),
+});
 
 export const queryType = new GraphQLObjectType({
   name: 'Query',
@@ -53,27 +94,52 @@ export const queryType = new GraphQLObjectType({
     memberTypes: {
       type: new GraphQLList(memberType),
       resolve: (
-        _source, 
+        parent, 
         args, 
         context:ContextInterface
-      ) => getAllMemberTypes(_source, args, context)
+      ) => getAllMemberTypes(parent, args, context)
     },
     posts: {
       type: new GraphQLList(postType),
       resolve: (
-        _source, 
+        parent, 
         args, 
         context:ContextInterface
-      ) => getAllPosts(_source, args, context)
+      ) => getAllPosts(parent, args, context)
     },
     post: {
       type: postType,
       args: { id: { type: new GraphQLNonNull(UUIDType) } },
       resolve: (
-        _source, 
-        args,
+        parent, 
+        args: { id:string },
         context:ContextInterface
-      ) => getPostByID(_source, args, context)
+      ) => getPostByID(parent, args, context)
+    },
+    profiles:{
+      type: new GraphQLList(profileType),
+      resolve: (
+        parent, 
+        args, 
+        context:ContextInterface
+      ) => getAllProfiles(parent, args, context)
+    },
+    users: {
+      type: new GraphQLList(userType),
+      resolve: (
+        parent, 
+        args, 
+        context:ContextInterface
+      ) => getAllUsers(parent, args, context)
+    },
+    user: {
+      type: userType,
+      args: { userId: { type: new GraphQLNonNull(UUIDType) } },
+      resolve: (
+        parent, 
+        args: { userId:string },
+        context:ContextInterface
+      ) => getUserByID(parent, args, context)
     }
   }),
 });
--- src/routes/graphql/query/resolvers.ts
+++ src/routes/graphql/query/resolvers.ts
@@ -1,23 +1,56 @@
 import { ContextInterface } from '../types/types.js';
 
 export const getAllMemberTypes = async (
-    _: any,
+    parent: any,
     args: any,
     context: ContextInterface
   ) => {
     return await context.prisma.memberType.findMany();
 };
 
+export const getAllProfiles = async (
+  parent: any,
+  args: any,
+  context: ContextInterface
+) => {
+  return await context.prisma.profile.findMany();
+};
+
 export const getAllPosts = async (
-  _: any,
+  parent: any,
   args: any,
   context: ContextInterface
 ) => {
   return await context.prisma.post.findMany();
 };
 
+export const getAllUsers = async (
+  parent: any,
+  args: any,
+  context: ContextInterface
+) => {
+  return await context.prisma.user.findMany();
+};
+
+export const getUserByID = async (
+  parent: any,
+  args: { userId:string },
+  context: ContextInterface
+)=> {
+  const user = await context.prisma.user.findUnique({
+    where: {
+      id: args.userId,
+    },
+  });
+  if (user === null) {
+    throw context.httpErrors.notFound();
+  }
+  return user;
+}
+
+
 export const getPostByID = async (
-  _: any,
+  parent: any,
   args: { id:string },
   context: ContextInterface
 )=> {
@@ -31,5 +64,36 @@ export const getPostByID = async (
   }
   return post;
 }
+
+
+export const getPostsByUserID = async (
+  parent: { userId: string },
+  args: any,
+  context: ContextInterface
+)=> {
+  return await context.prisma.post.findMany({
+    where: {
+      authorId: parent.userId,
+    },
+  });
+}
+
+export const getProfileByUserID = async (
+  parent: { userId: string },
+  args: any,
+  context: ContextInterface
+)=> {
+  const profile = await context.prisma.profile.findUnique({
+    where: {
+      userId: parent.userId,
+    },
+  });
+  if (profile === null) {
+    throw context.httpErrors.notFound();
+  }
+  return profile;
+}
+
+
 
 
